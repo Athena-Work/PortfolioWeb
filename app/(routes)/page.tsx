c43ace6d2fedb7e7b8618803fa0007a5f91da163
behradahp-work <behradahp.work@gmail.com>
2025-10-20 14:53:16 +0330

    add fonts

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,15 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div className=' bg-background'>
-      <span>Home</span>
-      <Link href='/about'>About page</Link>
+    <div className='flex flex-col p-10 gap-5 bg-background'>
+      <span className='text-6xl font-[100]'>Frontend Developer</span>
+      <span className='text-6xl font-[200]'>Frontend Developer</span>
+      <span className='text-6xl font-[300]'>Frontend Developer</span>
+      <span className='text-6xl font-[400]'>Frontend Developer</span>
+      <span className='text-6xl font-[500]'>Frontend Developer</span>
+      <span className='text-6xl font-[600]'>Frontend Developer</span>
+      <span className='text-6xl font-[700]'>Frontend Developer</span>
+      <span className='text-6xl font-[800]'>Frontend Developer</span>
+      <span className='text-6xl font-[900]'>Frontend Developer</span>
     </div>
   );
 }
